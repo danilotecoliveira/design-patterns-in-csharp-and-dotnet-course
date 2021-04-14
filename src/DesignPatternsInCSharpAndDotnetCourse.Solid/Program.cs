20ca58105e491bba67ffaccb617fe312d976884f
Danilo Oliveira <danilo.oliveira@alicredito.com.br>
2021-04-14 09:29:33 -0300

    Class about S of SOLID.

--- src/DesignPatternsInCSharpAndDotnetCourse.Solid/Program.cs
+++ src/DesignPatternsInCSharpAndDotnetCourse.Solid/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace DesignPatternsInCSharpAndDotnetCourse.Solid
 {
+    public class Journal
+    {
+        private readonly List<string> entries = new();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+
+            return count; // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, journal.ToString());
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var journal = new Journal();
+            journal.AddEntry("I cried today");
+            journal.AddEntry("I ate bug");
+
+            var persistence = new Persistence();
+
+            var fileName = @"c:\temp\journal.txt";
+
+            persistence.SaveToFile(journal, fileName, true);
+
+            // Process.Start(fileName);
+
+            Console.WriteLine(journal.ToString());
         }
     }
 }
