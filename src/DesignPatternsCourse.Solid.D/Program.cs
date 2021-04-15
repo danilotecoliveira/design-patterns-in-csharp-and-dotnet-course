a37d89fe13ed5611ddd2b9f0b2de1d22eb0a30c9
Danilo Oliveira <danilo.oliveira@alicredito.com.br>
2021-04-15 19:41:10 -0300

    Class about D of SOLID.

---
+++ src/DesignPatternsCourse.Solid.D/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatternsCourse.Solid.D
+{
+    class Program
+    {
+        public enum Relationship
+        {
+            Parent,
+            Child,
+            Sibling
+        }
+
+        public class Person
+        {
+            public string Name;
+        }
+
+        public interface IRelationshipBrowser
+        {
+            IEnumerable<Person> FindAllChildrenOf(string name);
+        }
+
+        public class Relationships : IRelationshipBrowser
+        {
+            private List<(Person, Relationship, Person)> relations = new();
+
+            public void AddParentAndChild(Person parent, Person child)
+            {
+                relations.Add((parent, Relationship.Parent, child));
+                relations.Add((child, Relationship.Child, parent));
+            }
+
+            public List<(Person, Relationship, Person)> Relations => relations;
+
+            public IEnumerable<Person> FindAllChildrenOf(string name)
+            {
+                return relations
+                  .Where(x => x.Item1.Name == name && x.Item2 == Relationship.Parent).Select(r => r.Item3);
+            }
+        }
+
+        public class Research
+        {
+            public Research(IRelationshipBrowser browser)
+            {
+                foreach (var p in browser.FindAllChildrenOf("John"))
+                {
+                    Console.WriteLine($"John has a child called {p.Name}");
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                var parent = new Person { Name = "John" };
+                var child1 = new Person { Name = "Chris" };
+                var child2 = new Person { Name = "Matt" };
+
+                var relationships = new Relationships();
+                relationships.AddParentAndChild(parent, child1);
+                relationships.AddParentAndChild(parent, child2);
+
+                _ = new Research(relationships);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
